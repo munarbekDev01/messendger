4fef5e3d346a4e79f9d535b00d1412764c1eec8d
munarbekDev01 <nokstor0008@gmail.com>
2025-03-20 16:04:21 +0600

    kjsnfndssf

--- src/app/(site)/(home)/chat/page.tsx
+++ src/app/(site)/(home)/chat/page.tsx
@@ -1,18 +1,29 @@
-"use client"
-import WebsocketChat from '@/components/pages/WebSocetPages/WebsocketChat';
-import { useSearchParams } from 'next/navigation';
-import React from 'react';
+"use client";
+import React, { useEffect, useState } from "react";
+import WebsocketChat from "@/components/pages/WebSocetPages/WebsocketChat";
+import { useSearchParams } from "next/navigation";
 
-const page = () => {
-     const searchParams = useSearchParams(); 
-      const stringData = searchParams.get("params"); 
-      const data = stringData ? JSON.parse(stringData) : null;
-      console.log(data);
-    return (
-        <div>
-            <WebsocketChat data={data}/>
-        </div>
-    );
+const Page = () => {
+  const searchParams = useSearchParams();
+  const [data, setData] = useState<{
+    key: string;
+    id: number;
+    user_name: string;
+  } | null>(null); // Type the state to allow null initially
+
+  useEffect(() => {
+    const stringData = searchParams.get("params");
+    const parsedData = stringData ? JSON.parse(stringData) : null;
+    if (parsedData) {
+      setData(parsedData); // Set the parsed data in state
+    }
+  }, [searchParams]);
+
+  return (
+    <div>
+      <WebsocketChat data={data || { key: "", id: 0, user_name: "" }} />
+    </div>
+  );
 };
 
-export default page;
+export default Page;
