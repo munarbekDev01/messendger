fafe46f5dbc3933d84f95254022702baa7858b3e
munarbekDev01 <nokstor0008@gmail.com>
2025-03-20 16:06:35 +0600

    sdfsf

--- src/app/(site)/(home)/chat/page.tsx
+++ src/app/(site)/(home)/chat/page.tsx
@@ -1,28 +1,19 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { Suspense } from "react";
 import WebsocketChat from "@/components/pages/WebSocetPages/WebsocketChat";
 import { useSearchParams } from "next/navigation";
 
 const Page = () => {
   const searchParams = useSearchParams();
-  const [data, setData] = useState<{
-    key: string;
-    id: number;
-    user_name: string;
-  } | null>(null); // Type the state to allow null initially
-
-  useEffect(() => {
-    const stringData = searchParams.get("params");
-    const parsedData = stringData ? JSON.parse(stringData) : null;
-    if (parsedData) {
-      setData(parsedData); // Set the parsed data in state
-    }
-  }, [searchParams]);
+  const stringData = searchParams.get("params");
+  const data = stringData ? JSON.parse(stringData) : null;
 
   return (
-    <div>
-      <WebsocketChat data={data || { key: "", id: 0, user_name: "" }} />
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div>
+        <WebsocketChat data={data || { key: "", id: 0, user_name: "" }} />
+      </div>
+    </Suspense>
   );
 };
 
